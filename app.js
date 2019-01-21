4e860368e7bd0bf6243628b3ca8679417fe8f68b
ebedoyaalzate <esteban.bedoyaa@udea.edu.co>
2019-01-20 22:14:59 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let salida = {
+            nombre: 'Esteban',
+            edad: '22',
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        // res.write('hola mundo');
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando en el puerto 8080');
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+
+const hbs = require('hbs')
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+
+//Express HBS engine
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/parciales');
+
+
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'Esteban'
+    });
+})
+
+app.get('/about', function(req, res) {
+    res.render('about')
+})
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones por el puerto ${port}`);
+})
